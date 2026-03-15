12f9c0ac30225164a2c52554592f882dc0613b15
VisualSource <collin_blosser@yahoo.com>
2026-03-15 18:05:24 -0400

    list

--- packages/desktop-client/src/routes/_text.tsx
+++ packages/desktop-client/src/routes/_text.tsx
@@ -1,4 +1,6 @@
+import { TooltipButton } from "@/components/ui/tooltip-button";
 import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { Hash, Pin } from "lucide-react";
 
 export const Route = createFileRoute("/_text")({
 	component: RouteComponent,
@@ -7,8 +9,20 @@ export const Route = createFileRoute("/_text")({
 function RouteComponent() {
 	return (
 		<div className="grid h-full w-full grid-cols-12">
-			<div className="col-span-10">
-				<Outlet />
+			<div className="col-span-10 flex flex-col h-full w-full">
+				<div className="h-10 bg-accent flex items-center px-2 justify-between border-b shadow">
+					<div className="flex gap-1 items-center">
+						<Hash className="h-4 w-4" /> Channel Name
+					</div>
+					<div>
+						<TooltipButton tooltip="Pins" variant="ghost" size="icon-lg">
+							<Pin />
+						</TooltipButton>
+					</div>
+				</div>
+				<div className="h-full w-full flex">
+					<Outlet />
+				</div>
 			</div>
 
 			<aside className="col-span-2 bg-sidebar"></aside>
--- packages/desktop-client/src/routes/_text/text.$roomId.tsx
+++ packages/desktop-client/src/routes/_text/text.$roomId.tsx
@@ -1,15 +1,178 @@
 import { Textarea } from "@/components/ui/textarea";
+import {
+	keepPreviousData,
+	useInfiniteQuery,
+	useMutation,
+} from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
+import { useVirtualizer } from "@tanstack/react-virtual";
+import Markdown from "react-markdown";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { faker } from "@faker-js/faker";
+import {
+	useActionState,
+	useCallback,
+	useEffect,
+	useEffectEvent,
+	useRef,
+} from "react";
+import { Button } from "@/components/ui/button";
+import { Send } from "lucide-react";
+import { Input } from "@/components/ui/input";
 
 export const Route = createFileRoute("/_text/text/$roomId")({
 	component: RouteComponent,
 });
 
+type Msg = {
+	timestamp: string;
+	userId: string;
+	message: string;
+	reacts: string[];
+};
+
+const VirtualList = ({
+	hasNextPage,
+	items,
+}: {
+	hasNextPage: boolean;
+	items: Msg[];
+}) => {
+	const count = items.length;
+
+	const initScrollRef = useRef(false);
+	const isAtBottom = useRef(false);
+
+	const listRef = useRef<HTMLDivElement>(null);
+
+	const virtualizer = useVirtualizer({
+		count: hasNextPage ? count + 1 : count,
+		getScrollElement: () => listRef.current,
+		estimateSize: () => 52,
+		measureElement:
+			navigator.userAgent.indexOf("Firefox") === -1
+				? (el) => el.getBoundingClientRect()?.height
+				: undefined,
+		overscan: 6,
+	});
+
+	const handleScroll = useCallback((ev: React.UIEvent<HTMLDivElement>) => {
+		const el = ev.currentTarget;
+		const distanceFromBottom =
+			el.scrollHeight - (el.scrollTop + el.clientHeight);
+		isAtBottom.current = distanceFromBottom < 8;
+	}, []);
+
+	const handleEventScroll = useEffectEvent(
+		(index: number, behavior?: ScrollBehavior) => {
+			virtualizer.scrollToIndex(index, { align: "end", behavior });
+		},
+	);
+
+	useEffect(() => {
+		if (!listRef.current) return;
+		if (count === 0) return;
+
+		if (!initScrollRef.current) {
+			initScrollRef.current = true;
+			handleEventScroll(count - 1, "instant");
+			return;
+		}
+
+		if (!isAtBottom.current) return;
+		handleEventScroll(count - 1, "smooth");
+	}, [count]);
+
+	return (
+		<div
+			ref={listRef}
+			className="h-[100cqh] w-[100-cqw] overflow-y-auto contain-strict"
+			style={{ overflowAnchor: "none" }}
+			onScroll={handleScroll}
+		>
+			<div className="relative" style={{ height: virtualizer.getTotalSize() }}>
+				{virtualizer.getVirtualItems().map((virtualRow) => {
+					const item = items[virtualRow.index];
+					return (
+						<div
+							onContextMenu={(ev) => ev.preventDefault()}
+							key={virtualRow.key}
+							data-index={virtualRow.index}
+							ref={virtualizer.measureElement}
+							className="absolute flex w-full hover:bg-accent/60 gap-2 px-2 py-1 cursor-pointer"
+							style={{ transform: `translateY(${virtualRow.start}px)` }}
+						>
+							<Avatar>
+								<AvatarFallback>CN</AvatarFallback>
+								<AvatarImage />
+							</Avatar>
+							<div className="flex flex-col">
+								<div className="flex gap-2 items-center align-middle">
+									<h1 className="hover:underline">Username</h1>
+									<div className="text-muted-foreground text-xs">
+										{item.timestamp}
+									</div>
+								</div>
+								<article className="text-sm">
+									<Markdown>{item.message}</Markdown>
+								</article>
+							</div>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
+};
+
 function RouteComponent() {
+	const { roomId } = Route.useParams();
+
+	const { data, isLoading, hasNextPage } = useInfiniteQuery({
+		queryKey: ["text-channel", roomId],
+		queryFn: ({ pageParam }) => {
+			return Array.from({ length: 200 }).map(() => ({
+				userId: faker.string.uuid(),
+				message: faker.lorem.text(),
+				reacts: [],
+				timestamp: faker.date.recent().toUTCString(),
+			})) as Msg[];
+		},
+		maxPages: 3,
+		getNextPageParam: () => undefined,
+		getPreviousPageParam: () => undefined,
+		initialPageParam: undefined,
+		placeholderData: keepPreviousData,
+		refetchOnWindowFocus: false,
+	});
+
+	const mutation = useMutation({
+		mutationKey: ["text-channel", "msg-mut", roomId],
+		mutationFn: async () => {
+			await new Promise((ok) => setTimeout(ok, 5000));
+
+			return {};
+		},
+	});
+
+	const items = data?.pages.flat() ?? [];
+
 	return (
 		<main className="container px-8 h-full flex flex-col pb-6 col-span-10">
-			<ul className="h-full"></ul>
-			<Textarea></Textarea>
+			<div className="h-full overflow-hidden @container-[size] mb-2">
+				<VirtualList hasNextPage={hasNextPage} items={items} />
+			</div>
+			<form
+				className="flex gap-1"
+				action={(data) => {
+					mutation.mutateAsync();
+				}}
+			>
+				<Input type="text" name="msg" />
+				<Button type="submit" disabled={mutation.isPending}>
+					<Send />
+				</Button>
+			</form>
 		</main>
 	);
 }
