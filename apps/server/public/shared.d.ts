5981264687bd52fd02b52c815bed115b28ed9214
Collin Blosser <blosserc@hurco.com>
2026-07-16 13:20:35 -0400

    start rework to use alpine

--- apps/server/public/shared.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const getErrorMessage: (target: HTMLInputElement) => string | null;
-export declare const reportError: (targets: string[]) => void;
-export declare const showMessage: (message: string) => void;
-export declare const handleErrorResponse: (response: Response) => Promise<void>;
-export declare const onFormSubmit: (target: string, callback: (formData: FormData) => void) => void;
-export declare const getRecaptchaToken: (siteKey: string, action: string) => Promise<string>;
-//# sourceMappingURL=shared.d.ts.map
