adf61079f665514590e7e24affb3d15c0034e453
Collin Blosser <blosserc@hurco.com>
2026-07-27 15:56:30 -0400

    queries

--- apps/server/src/models/channel.rs
+++ apps/server/src/models/channel.rs
@@ -1,10 +1,11 @@
 use serde::{Deserialize, Serialize};
-use sqlx::SqlitePool;
+use utoipa::ToSchema;
 use uuid::Uuid;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, ToSchema, sqlx::Type)]
+#[sqlx(type_name = "text", rename_all = "lowercase")]
 #[serde(rename_all = "lowercase")]
-enum ChannelKind {
+pub enum ChannelKind {
     Text,
     Voice,
     Dm,
@@ -12,40 +13,11 @@ enum ChannelKind {
 
 #[derive(Debug, Serialize, Deserialize, sqlx::FromRow)]
 pub struct Channel {
-    id: Uuid,
-    kind: ChannelKind,
-    server_id: Option<Uuid>,
-    name: String,
-    category: Option<String>,
-}
-
-impl Channel {
-    pub async fn create(
-        db: &SqlitePool,
-        server_id: Uuid,
-        kind: ChannelKind,
-        name: String,
-        category: Option<String>,
-    ) -> Result<Channel, sqlx::Error> {
-        todo!()
-    }
-    pub async fn delete(db: &SqlitePool, id: Uuid) -> Result<(), sqlx::Error> {
-        todo!()
-    }
-    pub async fn patch(
-        db: &SqlitePool,
-        name: String,
-        category: Option<String>,
-    ) -> Result<(), sqlx::Error> {
-        todo!()
-    }
-    pub async fn get(db: &SqlitePool, id: Uuid) -> Result<Channel, sqlx::Error> {
-        todo!()
-    }
-
-    pub async fn get_All_by_server(db: &SqlitePool) -> Result<Vec<Channel>, sqlx::Error> {
-        todo!()
-    }
+    pub id: Uuid,
+    pub kind: ChannelKind,
+    pub server_id: Option<Uuid>,
+    pub name: String,
+    pub category: Option<String>,
 }
 
 /// link info for a channel(dm)
@@ -56,24 +28,6 @@ pub struct DmParticipant {
     user_a_id: Uuid,
     user_b_id: Uuid,
 }
-
-impl DmParticipant {
-    pub async fn create(
-        db: &SqlitePool,
-        channel_id: Uuid,
-        user_a: Uuid,
-        user_b: Uuid,
-    ) -> Result<DmParticipant, sqlx::Error> {
-        todo!()
-    }
-    pub async fn delete(db: &SqlitePool, id: String) -> Result<(), sqlx::Error> {
-        todo!()
-    }
-    pub async fn get(db: &SqlitePool, id: String) -> Result<DmParticipant, sqlx::Error> {
-        todo!()
-    }
-}
-
 #[derive(Debug, Serialize, Deserialize, sqlx::FromRow, utoipa::ToSchema)]
 pub struct Message {
     id: Uuid,
@@ -86,12 +40,4 @@ pub struct Message {
     edited_at: Option<time::OffsetDateTime>,
     #[serde(with = "time::serde::rfc3339::option")]
     delete_at: Option<time::OffsetDateTime>,
-}
-
-impl Message {
-    pub async fn create() {}
-    pub async fn get() {}
-
-    pub async fn delete() {}
-    pub async fn patch() {}
 }
--- apps/server/src/routes/api/channel.rs
+++ apps/server/src/routes/api/channel.rs
@@ -1,1 +1,47 @@
-use actix_web::{HttpResponse, Responder, delete, get, patch, post};
+use actix_web::{HttpResponse, Responder, delete, get, patch, post, web};
+use serde::Deserialize;
+use sqlx::{SqlitePool, query_as};
+use utoipa::ToSchema;
+use uuid::Uuid;
+use validator::Validate;
+
+use crate::{
+    models::channel::{Channel, ChannelKind},
+    state::{api_errors::ApplicationError, oauth::jwt::Claims},
+};
+
+#[derive(Debug, Deserialize, Validate, ToSchema)]
+struct CreateChannelPayload {
+    #[validate(length(min = 3, max = 255))]
+    name: String,
+    #[validate(length(min = 3, max = 255))]
+    category: Option<String>,
+
+    kind: ChannelKind,
+
+    server_id: Uuid,
+}
+
+#[post("/channel")]
+pub async fn create_channel(
+    db: web::Data<SqlitePool>,
+    body: web::Json<CreateChannelPayload>,
+    user: web::ReqData<Claims>,
+) -> Result<web::Json<Channel>, ApplicationError> {
+    //TODO: validate user can make channel on given server
+
+    let id = uuid::Uuid::now_v7();
+    let channel = query_as!(
+        Channel,
+        "INSERT INTO channels VALUES (?,?,?,?,?) RETURNING *",
+        id,
+        body.kind,
+        body.server_id,
+        body.name,
+        body.category
+    )
+    .fetch_one(db.get_ref())
+    .await?;
+
+    Ok(web::Json(channel))
+}
